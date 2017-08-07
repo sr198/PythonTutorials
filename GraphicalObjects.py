71f544cb8e2cae0fed8dd229698c2c4de898eefb
Sr198 <shreejan@gmail.com>
2017-08-07 13:04:45 -0400

    Update to GraphicalObjects.py

--- GraphicalObjects.py
+++ GraphicalObjects.py
@@ -53,24 +53,118 @@ def draw_triangle():
     start.shape("arrow")
     start.speed(2)
     start.color("blue")
+    window = turtle.Screen()
+    window.bgcolor("red")
+    
+    start = turtle.Turtle()
+    start.shape("turtle")
+    start.speed(50)
+    start.color("white")
 
     count = 0
-    while( count < 3 ):
-        start.backward(100)
-        start.left(120)
+    while( count < 180 ):        
+        start.forward(100)
+        start.right(90)
+
+        start.forward(100)
+        start.right(90)
+
+        start.forward(100)
+        start.right(90)
+
+        start.forward(100)
+        start.right(90)
+
+        start.right(2)
         count = count + 1
-    
 
-
-def main_program():
-    window = turtle.Screen()
-    window.bgcolor("grey")
-    
-    # draw a square
-    draw_square()
-    draw_circle()
-    draw_triangle()
-    draw_square_fractal()
     window.exitonclick()
+
+
+def draw_triangle_fractal(startX, startY, edgeSize):
+    window = turtle.Screen()
+    window.bgcolor("white")
+    
+    start = turtle.Turtle()
+    start.shape("turtle")
+    start.speed(1)
+    start.color("red")
+
+    #draw base triangle first
+    start.penup()
+    start.setpos(startX, startY)
+    
+    start.pendown()
+    start.forward(edgeSize)
+    start.left(120)
+    start.forward(edgeSize)
+    start.left(120)
+    start.forward(edgeSize)
+    
+    start.left(180)
+    start.forward(edgeSize/2)
+    start.right(60)
+
+    start.forward(edgeSize/2)
+    start.right(120)
+    start.forward(edgeSize/2)
+    start.right(120)
+    start.forward(edgeSize/2)
+
+    start.penup()
+    start.backward(edgeSize/4)
+    start.pendown()
+    draw_triangle(start, edgeSize/4, 1)
+##
+##    start.penup()
+##    start.setx(start.xcor()+(edgeSize/4))
+##    start.pendown()
+##    draw_triangle(start, edgeSize/4, "right",1)
+##
+##    start.penup()
+##    start.forward(edgeSize/4)    
+##    start.pendown()
+##    draw_triangle(start, edgeSize/4, "top",1)
+##
+##    start.penup()
+##    start.
+##    start.pendown()
+    window.exitonclick()
+
+def draw_triangle(start, edge, level):
+    start.setheading(0)
+    level = level + 1
+
+    if( level == 3 ):
+        return
+    
+    #draw triangle on the left
+    start.left(180)
+    start.forward(edge)
+    start.left(120)
+    start.forward(edge)
+    start.left(120)
+    start.forward(edge)
+    draw_triangle( start, edge/2, level)
+
+    #triangle on the right
+    start.forward(edge)
+    start.right(120)
+    start.forward(edge)
+    start.right(120)
+    start.forward(edge)
+    draw_triangle( start, edge/2, level)
+    
+    #triangle on the top
+    start.left(60)
+    start.forward(edge)
+    start.left(120)
+    start.forward(edge)
+    start.left(120)
+    start.forward(edge)
+    draw_triangle( start, edge/2, level)
+    
+def main_program():
+   draw_triangle_fractal(0.0,0.0,400)
     
 main_program()
